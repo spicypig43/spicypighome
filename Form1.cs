6d37bead7ecb1a774ca9aac044952b455cf612a8
spicypig43 <164742773+spicypig43@users.noreply.github.com>
2024-03-24 19:52:34 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
